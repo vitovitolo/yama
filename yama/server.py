23a95987140c29b4cf6432281c1d5fe7ffe0bf29
root <root@debian>
2016-12-01 20:21:39 +0000

    Added URLINFO route in server module: parse URL properly and response it

--- yama/server.py
+++ yama/server.py
@@ -7,8 +7,7 @@
     YAMA HTTP web server based in bottle framework
 
 """
-
-from bottle import run, route
+from bottle import run, route, request
 
 @route('/status')
 def status():
@@ -18,6 +17,16 @@ def status():
 @route('/reload_config')
 def reload_config():
     response = { "status": "config reloaded" }
+    return response
+
+@route('/urlinfo/1/<hostname>\:<port:int>/<path_query:re:.*>')
+def urlinfo(hostname, port, path_query):
+    # we have to split the full path request
+    # in order to trim 'urlinfo/1/<hostname>:<port>
+    path = str(request.fullpath).split(':',1)[1].split('/',1)[1]
+    # The query does not have any character after '#'
+    query = str(request.query_string)
+    response = { "hostname": str(hostname), "port": port, "full_path": path+"?"+query }
     return response
 
 def start(host, port):

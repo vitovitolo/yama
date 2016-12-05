1cf27c3331837e6d5411377485fda67ef188008d
Victor <victor.torre@gmail.com>
2016-12-05 15:36:23 +0100

    Added MIT License and fixes some bugs in setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
-setup(name='Yama',
+setup(name='yama',
       version='0.1.0',
       description='Yet Another Malware Analyzer',
       author='Victor Torre',
       author_email='victor.torre@gmail.com',
       url='https://github.com/vitovitolo/yama',
       packages=['yama'],
+      license='LICENSE.txt',
+      install_requires=['bottle>=0.12', 'pyyaml>=3.11', 'mysql-python>=1.2.3'],
      )

bd83e8e78b33d0d3e5b189e59b1d7474506b43c6
Victor <victor.torre@gmail.com>
2016-12-05 12:34:08 +0100

    Improved url file generator scripts to use with siege

--- utils/url_file_generator.sh
+++ utils/url_file_generator.sh
@@ -5,26 +5,27 @@
 MYNAME=`basename $0`
 
 
-if [ $# -ne 4 ]; then
-	echo "usage: ${MYNAME} <output file> <format> <GET op> <random op>"
+if [ $# -ne 3 ]; then
+	echo "usage: ${MYNAME} <output file> <format> <opeartion>"
 	echo "<output file>: output filename to write URL list"
 	echo "<format>: csv or http"
-	echo "<GET op>: GET operation to generate in URL: info, del or add"
-	echo "<random op>: random operation in URL: del and adds"
+	echo "<operation>: GET operation to generate in URL: info, del, add or random"
 	exit 1
 else
 	FILENAME=`basename ${1}`
 	FORMAT=${2}
 	GET_OP=${3}
-	RANDOM_OP=${4}
 fi
 
 while true; do
 	FIRST=`pwgen 5 1`
 	DOMAIN=`pwgen $((RANDOM%20)) 1`
-	PORT=$(($RANDOM%80+8000))
+	PORTS=(80 443 8080)
+	I_PORT=$(($RANDOM%3))
+	PATH_RAND=`pwgen $((RANDOM%200+10)) 1`
+	QUERY_RAND=`pwgen $((RANDOM%60+10)) 1`
 	if [ "${FORMAT}" == "http" ]; then
-		if [ "${RANDOM_OP}" == "yes" ]; then
+		if [ "${GET_OP}" == "random" ]; then
 			OPERATION=$(($RANDOM%3))
 			if [ ${OPERATION} == "0" ]; then
 				OP="urlinfo"
@@ -46,7 +47,7 @@ while true; do
 			OP="urlupdate"
 			OP2="del"
 		fi
-		LINE="http://localhost/${OP}/${OP2}/${FIRST}${DOMAIN}:${PORT}/path/to/something/index.html"
+		LINE="http://localhost/${OP}/${OP2}/${FIRST}${DOMAIN}:${PORTS[${I_PORT}]}/path/to/${PATH_RAND}/${QUERY_RAND}"
 	# Format == csv
 	else
 		LINE="${FIRST}${DOMAIN},${PORT},/path/to/something/index.html"

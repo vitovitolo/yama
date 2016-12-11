5ddc0ceb0e922de9143c98806d2be6b30984109c
Victor <victor.torre@gmail.com>
2016-12-11 10:41:10 +0100

    Fixed dependency error in puppet mysqlserver module: applysql needs sql file before exec

--- puppet/modules/mysqlserver/manifests/applysql.pp
+++ puppet/modules/mysqlserver/manifests/applysql.pp
@@ -14,8 +14,9 @@ define mysqlserver::applysql
 		content => $sql_content,
 	}
 	exec {"apply_schema_${name}":
-		path    => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/sbin:/usr/sbin:/usr/local/sbin",
-		command => "mysql -u${db_user} -p${db_pass} < /tmp/.sql_${name} && touch /root/.apply_schema_${name}",
+		path     => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/sbin:/usr/sbin:/usr/local/sbin",
+		command  => "mysql -u${db_user} -p${db_pass} < /tmp/.sql_${name} && touch /root/.apply_schema_${name}",
+		requires => "/tmp/.sql_${name}",
 		creates  => "/root/.apply_schema_${name}",
 	}
 }
